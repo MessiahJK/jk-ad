850029054f94db66c509823ccf9e27c3aaad9538
MessiahJK <jk@linkmun.com>
2021-04-07 21:16:32 +0800

    CreateUser Service finish

--- ad-service/ad-sponsor/src/main/java/us/betahouse/ad/service/IUserService.java
+++ ad-service/ad-sponsor/src/main/java/us/betahouse/ad/service/IUserService.java
@@ -1,5 +1,17 @@
 package us.betahouse.ad.service;
 
+import us.betahouse.ad.exception.AdException;
+import us.betahouse.ad.vo.CreateUserRequest;
+import us.betahouse.ad.vo.CreateUserResponse;
+
 public interface IUserService {
 
+    /**
+     * 创建用户
+     *
+     * @param createUserRequest 创建用户请求
+     * @return CreateUserResponse
+     * @throws AdException 广告异常
+     */
+    CreateUserResponse createUser(CreateUserRequest createUserRequest) throws AdException;
 }
---
+++ ad-service/ad-sponsor/src/main/java/us/betahouse/ad/service/impl/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package us.betahouse.ad.service.impl;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import us.betahouse.ad.constant.Constants;
+import us.betahouse.ad.dao.AdUserRepository;
+import us.betahouse.ad.entity.AdUser;
+import us.betahouse.ad.exception.AdException;
+import us.betahouse.ad.service.IUserService;
+import us.betahouse.ad.utils.CommonUtils;
+import us.betahouse.ad.vo.CreateUserRequest;
+import us.betahouse.ad.vo.CreateUserResponse;
+
+import javax.transaction.Transactional;
+
+public class UserServiceImpl implements IUserService {
+
+    private final AdUserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(AdUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public CreateUserResponse createUser(CreateUserRequest request)
+            throws AdException {
+
+        if (!request.validate()) {
+            throw new AdException(Constants.ErrorMsg.REQUEST_PARAM_ERROR);
+        }
+
+        AdUser oldUser = userRepository.
+                findByUsername(request.getUsername());
+        if (oldUser != null) {
+            throw new AdException(Constants.ErrorMsg.SAME_NAME_ERROR);
+        }
+
+        AdUser newUser = userRepository.save(new AdUser(
+                request.getUsername(),
+                CommonUtils.md5(request.getUsername())
+        ));
+
+        return new CreateUserResponse(
+                newUser.getId(), newUser.getUsername(), newUser.getToken(),
+                newUser.getCreateTime(), newUser.getUpdateTime()
+        );
+    }
+}
