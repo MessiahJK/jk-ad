8887614ba6704a765dcc54a3b607ddda14adb318
MessiahJK <34203586+MessiahJK@users.noreply.github.com>
2021-05-10 01:33:51 +0800

    Creative unit index finish

---
+++ ad-service/ad-search/src/main/java/us/betahouse/ad/index/creativeunit/CreativeUnitObject.java
@@ -0,0 +1,17 @@
+package us.betahouse.ad.index.creativeunit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitObject {
+
+    private Long adId;
+    private Long unitId;
+
+    // adId-unitId
+}
