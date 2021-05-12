e63d554bf95a5e43698baa6b815346a022ba30e1
MessiahJK <34203586+MessiahJK@users.noreply.github.com>
2021-05-13 01:44:16 +0800

    level 3 handle finish

--- ad-service/ad-search/src/main/java/us/betahouse/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/us/betahouse/ad/handler/AdLevelDataHandler.java
@@ -1,15 +1,25 @@
 package us.betahouse.ad.handler;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import us.betahouse.ad.dump.table.AdCreativeTable;
+import us.betahouse.ad.dump.table.AdCreativeUnitTable;
 import us.betahouse.ad.dump.table.AdPlanTable;
+import us.betahouse.ad.dump.table.AdUnitTable;
 import us.betahouse.ad.index.DataTable;
 import us.betahouse.ad.index.IndexAware;
 import us.betahouse.ad.index.adplan.AdPlanIndex;
 import us.betahouse.ad.index.adplan.AdPlanObject;
+import us.betahouse.ad.index.adunit.AdUnitIndex;
+import us.betahouse.ad.index.adunit.AdUnitObject;
 import us.betahouse.ad.index.creative.CreativeIndex;
 import us.betahouse.ad.index.creative.CreativeObject;
+import us.betahouse.ad.index.creativeunit.CreativeUnitIndex;
+import us.betahouse.ad.index.creativeunit.CreativeUnitObject;
 import us.betahouse.ad.mysql.constant.OpType;
+import us.betahouse.ad.utils.CommonUtils;
 
+@Slf4j
 public class AdLevelDataHandler {
 
     public static void handleLevel2(AdPlanTable planTable, OpType type) {
@@ -45,6 +55,69 @@ public class AdLevelDataHandler {
                 DataTable.of(CreativeIndex.class),
                 creativeObject.getAdId(),
                 creativeObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
+
+        AdPlanObject adPlanObject = DataTable.of(
+                AdPlanIndex.class
+        ).get(unitTable.getPlanId());
+        if (null == adPlanObject) {
+            log.error("handleLevel3 found AdPlanObject error: {}",
+                    unitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject unitObject = new AdUnitObject(
+                unitTable.getUnitId(),
+                unitTable.getUnitStatus(),
+                unitTable.getPositionType(),
+                unitTable.getPlanId(),
+                adPlanObject
+        );
+
+        handleBinlogEvent(
+                DataTable.of(AdUnitIndex.class),
+                unitTable.getUnitId(),
+                unitObject,
+                type
+        );
+    }
+
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable,
+                                    OpType type) {
+
+        if (type == OpType.UPDATE) {
+            log.error("CreativeUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(
+                AdUnitIndex.class
+        ).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(
+                CreativeIndex.class
+        ).get(creativeUnitTable.getAdId());
+
+        if (null == unitObject || null == creativeObject) {
+            log.error("AdCreativeUnitTable index error: {}",
+                    JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(
+                creativeUnitTable.getAdId(),
+                creativeUnitTable.getUnitId()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(
+                        creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()
+                ),
+                creativeUnitObject,
                 type
         );
     }
