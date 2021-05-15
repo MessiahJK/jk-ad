dc3332af673c0b472cb45bd4d1ff7b6390927772
MessiahJK <34203586+MessiahJK@users.noreply.github.com>
2021-05-15 23:59:31 +0800

    template json and some class finish

---
+++ ad-service/ad-search/src/main/java/us/betahouse/ad/mysql/dto/Template.java
@@ -0,0 +1,16 @@
+package us.betahouse.ad.mysql.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Template {
+
+    private String database;
+    private List<JsonTable> tableList;
+}
